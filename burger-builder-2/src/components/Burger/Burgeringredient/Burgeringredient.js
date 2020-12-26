0dd551264f9ade3b689a353d8df2612d7df8bacd
anukulSaini <anukulsaini2295@gmail.com>
2020-12-26 10:59:06 +0530

    add burger components

---
+++ burger-builder-2/src/components/Burger/Burgeringredient/Burgeringredient.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import BurgerBuilder from '../../../containers/BurgerBuilder/BurgerBuilder';
+
+const Burgeringredient = (props) => {
+
+}
+
+export default Burgeringredient;
