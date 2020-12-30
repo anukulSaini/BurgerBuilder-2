b2531a23ab55c80c63e570fe682355e9cb37f227
anukulSaini <anukulsaini2295@gmail.com>
2020-12-30 13:08:00 +0530

    adding build controls

---
+++ burger-builder-2/src/components/Burger/Buildcontrols/BuildControl/BuildControl.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+
+const BuildControl = (props) => (
+     <div>
+         <div>{props.label}</div>
+         <button>Less</button>
+         <button>More</button>
+     </div>
+
+);
+
+
+export default BuildControl;
