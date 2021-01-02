e2610d4eca988dfba5fa5ab262f1c9721686f313
anukulSaini <anukulsaini2295@gmail.com>
2021-01-03 03:21:40 +0530

    show price

--- burger-builder-2/src/components/Burger/Buildcontrols/Buildcontrols.js
+++ burger-builder-2/src/components/Burger/Buildcontrols/Buildcontrols.js
@@ -13,6 +13,7 @@ const Controls =[
 
 const buildcontrols = (props) => (
    <div className={classes.BuildControls}>
+      <p>Current Price : <strong>{props.price.toFixed(2)}</strong></p>
        {Controls.map(ctrl =>(
           <BuildControl 
           key={ctrl.label} 
