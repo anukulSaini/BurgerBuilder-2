f7a00f2ec337ab9636c6ba42e348b776254da7f1
anukulSaini <anukulsaini2293@gmail.com>
2021-01-07 10:39:56 +0530

    add logo.css file and imported to toolbar.js

--- burger-builder-2/src/components/Logo/Logo.js
+++ burger-builder-2/src/components/Logo/Logo.js
@@ -1,5 +1,14 @@
 import React from 'react';
-  
-const logo = (props)  => ();
+ 
+import classes from './Logo.css';
+import burgerLogo from '../../assets/images/burger-logo.png'
+
+const logo = (props)  => (
+      <div className={classes.Logo}>
+          <img src = {burgerLogo} alt = "MyBurger"></img>
+      
+      </div>
+
+);
 
 export default logo;
