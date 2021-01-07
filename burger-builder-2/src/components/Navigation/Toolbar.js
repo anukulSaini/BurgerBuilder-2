5a4c5c61755f17ccd9580fe3ab85c333a8889e35
anukulSaini <anukulsaini2293@gmail.com>
2021-01-07 09:15:22 +0530

    added basic toolbar component

---
+++ burger-builder-2/src/components/Navigation/Toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const toolbar = (props) =>(
+    <header>
+        <div>MENU</div>
+        <di>Logo</di>
+        <nav>.....</nav>
+    </header>
+);
+
+export default toolbar;
