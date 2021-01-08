22d9808369f445f5bbe9f9d83d09999886259c2b
anukulSaini <anukulsaini2293@gmail.com>
2021-01-08 19:56:45 +0530

    updated sideDrwer.js and sideDrawer.css

--- burger-builder-2/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder-2/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './sideDrawer.css';
+import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
     return (
-        <div>
+        <div className ={classes.SideDrawer}>
              <Logo/>
              <nav>
                  <NavigationItems/>
