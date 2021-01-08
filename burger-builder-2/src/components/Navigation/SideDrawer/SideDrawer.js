eb3b59631acf4b7901e19314bf645088cd08b5dc
anukulSaini <anukulsaini2293@gmail.com>
2021-01-08 19:30:35 +0530

    added sideDrawer.css

--- burger-builder-2/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder-2/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './sideDrawer.css';
 
 const sideDrawer = (props) => {
     return (
