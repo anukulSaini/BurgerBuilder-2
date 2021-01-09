aef79cc4398d54bec7e96832e5504615ade03c1c
anukulSaini <anukulsaini2293@gmail.com>
2021-01-09 11:37:51 +0530

    make layout a class based components

--- burger-builder-2/src/components/Layout/Layout.js
+++ burger-builder-2/src/components/Layout/Layout.js
@@ -1,20 +1,24 @@
 
-import React  from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => ( 
-   <Aux>
-     <Toolbar/>
-     <SideDrawer/>
+class Layout extends Component{
+    render (){
+        return (
+            <Aux>
+            <Toolbar/>
+            <SideDrawer/>
+       
+           <main className={classes.Content}>
+               {this.props.children}
+           </main>
+           </Aux>
+        )
+    }
+} 
 
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </Aux>
-);
-
-export default layout;
+export default Layout;
--- burger-builder-2/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder-2/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,10 +3,14 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
     return (
-        <div className ={classes.SideDrawer}>
+       <Aux>
+           <Backdrop show></Backdrop>
+            <div className ={classes.SideDrawer}>
              <div className={classes.Logo}>
                 <Logo/>
              </div>
@@ -14,6 +18,7 @@ const sideDrawer = (props) => {
                  <NavigationItems/>
              </nav>
         </div>
+       </Aux>
     );
 };
 
