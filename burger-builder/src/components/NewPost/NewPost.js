e1bda1a0999d959ecbe802d20d9f45f78294b659
anukulSaini <anukulsaini2293@gmail.com>
2021-01-13 10:34:49 +0530

    send data request to a server using axios

--- burger-builder/src/components/NewPost/NewPost.js
+++ burger-builder/src/components/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -8,6 +9,19 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
+
+    postDataHAndler = () => {
+        const post = {
+            title :this.state.title,
+            body : this.state.content,
+            author :this.state.author
+        };
+       axios.post ('https://jsonplaceholder.typicode.com/posts',post)
+       .then(response =>{
+           console.log(response);
+       })
+    }
+
 
     render () {
         return (
@@ -22,7 +36,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHAndler} >Add Post</button>
             </div>
         );
     }
