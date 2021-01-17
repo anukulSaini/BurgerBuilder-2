cf7855426e6b224cb0cacefd4ddc8bdc76e93362
anukulSaini <anukulsaini2295@gmail.com>
2021-01-17 19:46:21 +0530

    updated project

--- burger-builder/src/components/FullPost/FullPost.js
+++ burger-builder/src/components/FullPost/FullPost.js
@@ -11,13 +11,20 @@ class FullPost extends Component {
     componentDidUpdate () {
         if ( this.props.id ) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
-                axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.id )
+                axios.get( '/posts/' + this.props.id )
                     .then( response => {
                         // console.log(response);
                         this.setState( { loadedPost: response.data } );
                     } );
             }
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
@@ -29,9 +36,9 @@ class FullPost extends Component {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.content}</p>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
--- burger-builder/src/containers/Blog/Blog.js
+++ burger-builder/src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -9,11 +10,12 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount () {
-        axios.get( 'https://jsonplaceholder.typicode.com/posts' )
+        axios.get( '/posts' )
             .then( response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
@@ -24,7 +26,11 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 // console.log( response );
-            } );
+            } )
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true});
+            });
     }
 
     postSelectedHandler = (id) => {
@@ -32,13 +38,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />;
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        }
 
         return (
             <div>
