f131d976991400f6d3381fdd35c12bbdfcede92a
anukulSaini <anukulsaini2295@gmail.com>
2021-01-24 10:41:56 +0530

    added style to CSummary.css and imported in jsx component

--- burger-builder-2/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder-2/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Burger from '../../Burger/Burger'
 import Button from '../../UI/Button/Button'
+import Classes from './CheckoutSummary.css';
 
 const chekoutSummary = (props) => {
  
         return ( 
-            <div>
+            <div className={Classes.CheckoutSummary}>
                 <h1>We hope it tastes well!!!</h1>
                 <div style ={{width:'300px', height: '300px' , margin :'auto'}}>
                      <Burger  ingredients ={props.ingredients}/>
