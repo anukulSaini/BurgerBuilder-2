441352ac488b64de5fe20f4c17f53177e6d17290
anukulSaini <anukulsaini2295@gmail.com>
2021-01-24 10:48:27 +0530

    imported  CS component to Checkout component

--- burger-builder-2/src/containers/Checkout/Checkout.js
+++ burger-builder-2/src/containers/Checkout/Checkout.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
 class Checkout extends Component {
-    state = {  }
     render() { 
-        return (  );
+        return ( 
+            <div>
+                <CheckoutSummary ingredients/>
+            </div>
+         );
     }
 }
  
