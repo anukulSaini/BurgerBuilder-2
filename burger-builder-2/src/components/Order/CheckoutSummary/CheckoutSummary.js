ecfd5efbde97497faa2a8fa4762d285aa224fcaa
anukulSaini <anukulsaini2295@gmail.com>
2021-01-24 10:22:17 +0530

    imported burger.js to CheckoutSummary

--- burger-builder-2/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder-2/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import Burger from '../../Burger/Burger'
 
 const chekoutSummary = (props) => {
  
-        return (  );
+        return ( 
+            <div>
+                <h1>We hope it tastes well!!!</h1>
+                <div>
+                     <Burger/>
+                </div>
+            </div>
+         );
     
 }
  
