5e2cc722024a75a7d0190fd89c00a24382dffaf8
anukulSaini <anukulsaini2295@gmail.com>
2021-01-24 10:31:07 +0530

    added style + button component to checkoutSUmmary

--- burger-builder-2/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder-2/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Burger from '../../Burger/Burger'
+import Button from '../../UI/Button/Button'
 
 const chekoutSummary = (props) => {
  
         return ( 
             <div>
                 <h1>We hope it tastes well!!!</h1>
-                <div>
-                     <Burger/>
+                <div style ={{width:'300px', height: '300px' , margin :'auto'}}>
+                     <Burger  ingredients ={props.ingredients}/>
                 </div>
+                <Button/>
             </div>
          );
     
