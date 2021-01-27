6a4a5bae1b9f8a8200c34b0f444947d432917c5a
anukulSaini <anukulsaini2295@gmail.com>
2021-01-27 19:03:16 +0530

    fixing bug

--- burger-builder-2/src/containers/Checkout/Checkout.js
+++ burger-builder-2/src/containers/Checkout/Checkout.js
@@ -6,23 +6,25 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
     state ={
-        ingredients :{
-            salad:1,
-            meat:1,
-            cheese:1,
-            bacon:1
-        }
+        ingredients :null,
+        price :0
     }
 
 
-    // componentDidMount() {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients ={};
-    //     for (let param of query.entries()){
-    //       ingredients[param[0]] = +param[1];  
-    //     }
-    //     this.setState({ingredients:ingredients});
-    // }
+    componentWillMount () {
+        const query = new URLSearchParams( this.props.location.search );
+        const ingredients = {};
+        let price = 0;
+        for ( let param of query.entries() ) {
+            // ['salad', '1']
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
+        }
+        this.setState( { ingredients: ingredients, totalPrice: price } );
+    }
 
 
     chekouContinuedHandler= () =>{
@@ -43,7 +45,7 @@ class Checkout extends Component {
                 chekoutCancelled={this.checkoutCancelledHandler}
                 chekouContinued={this.chekouContinuedHandler}/>
                 <Route Path={this.props.match.path = '/contact-data'}
-                   render ={() =>(<ContactData  ingredients={this.state.ingredients}/>)}
+                   render ={() =>(<ContactData  ingredients={this.state.ingredients}  price={this.state.totalPrice} {...props}/>)}
                  >
                 </Route>
             </div>
