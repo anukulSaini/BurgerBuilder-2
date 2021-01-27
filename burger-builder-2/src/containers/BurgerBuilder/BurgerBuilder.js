06369e0856c28efc383ed471ec87f0ba8e5a9713
anukulSaini <anukulsaini2295@gmail.com>
2021-01-27 11:20:46 +0530

    continue route****

--- burger-builder-2/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-2/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,7 +81,18 @@ class BurgerBuilder extends Component {
     //     .catch (error => {
     //         this.setState({Loading:false,purchasing:false});
     //     });
-         this.props.history.push('/checkout');
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '==' +  encodeURIComponent(this.state.ingredients[i]));
+        } 
+
+        const queryString = queryParams.join('+');
+
+
+         this.props.history.push({
+             pathname:'/checkout',
+             search : '?' + queryString
+         });
     
     }
 
