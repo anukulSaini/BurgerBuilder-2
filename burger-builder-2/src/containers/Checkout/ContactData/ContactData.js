925cd3e1d6970340d7c0011bf1cbfcfd939fe722
anukulSaini <anukulsaini2295@gmail.com>
2021-01-27 12:35:57 +0530

    added state to contactdata and style to cd.css file

--- burger-builder-2/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-2/src/containers/Checkout/ContactData/ContactData.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+// import Spinner from '../../../components/UI/Spinner/Spinner';
+// import classes from './ContactData.css';
+// import axios from '../../../axios-orders';
+
 class ContactData extends Component {
-    state = {  }
-    render() { 
-        return (  );
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        },
+        loading: false
+    }
+
+    render () {
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+       
     }
 }
- 
+
 export default ContactData;
