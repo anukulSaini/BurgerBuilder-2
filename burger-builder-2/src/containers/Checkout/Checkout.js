dd3d73258696d16f4402cecf66739134a275b831
anukulSaini <anukulsaini2295@gmail.com>
2021-01-27 12:12:18 +0530

    bug

--- burger-builder-2/src/containers/Checkout/Checkout.js
+++ burger-builder-2/src/containers/Checkout/Checkout.js
@@ -7,9 +7,22 @@ class Checkout extends Component {
         ingredients :{
             salad:1,
             meat:1,
-            cheese:1
+            cheese:1,
+            bacon:1
         }
     }
+
+
+    // componentDidMount() {
+    //     const query = new URLSearchParams(this.props.location.search);
+    //     const ingredients ={};
+    //     for (let param of query.entries()){
+    //       ingredients[param[0]] = +param[1];  
+    //     }
+    //     this.setState({ingredients:ingredients});
+    // }
+
+
     chekouContinuedHandler= () =>{
         this.props.history.replace('/checkout/contact-data');
     }
