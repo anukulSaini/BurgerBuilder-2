a8d35f6e7676fa08b92b8698d4a59a97fd3f21bb
anukulSaini <anukulsaini2295@gmail.com>
2021-01-29 21:40:58 +0530

    fixed a bug

--- burger-builder-2/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder-2/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import classes from './navigationItem.css';
+import {NavLink} from 'react-router-dom';
+
 
 const navigationItem = (props) =>(
     <li className={classes.NavigationItem}>
-        <a
-         href={props.link}
-         className={props.active ? classes.active : null}
-        >{props.children}</a>
+        <NavLink
+         to={props.link}
+         exact = {props.exact}
+         activeClassName={classes.active}
+        >{props.children}</NavLink>
         </li>
 );
 
