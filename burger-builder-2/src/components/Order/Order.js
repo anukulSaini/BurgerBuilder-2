add8ec9be23756fd570641097f2ffb2e4a8e67ac
anukulSaini <anukulsaini2295@gmail.com>
2021-01-29 19:55:47 +0530

    fixed a bug

--- burger-builder-2/src/components/Order/Order.js
+++ burger-builder-2/src/components/Order/Order.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Classes from './Order.js'
+import Classes from './Order.css'
 
 const  order =(props) => {
     return(
           <div className={Classes.Order}>
               <p>ingredients: salad(1)</p>
-              <p>ingredients: salad(2)</p>
+              <p>Price:<strong>10</strong></p>
 
           </div>
     );
