45be574a71d8339f522b96042e8bfd18a3325cb2
anukulSaini <anukulsaini2295@gmail.com>
2021-01-29 21:41:25 +0530

    fixed stylig bug in navigation

--- burger-builder-2/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-builder-2/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
      <ul className={classes.NavigationItems}>
-        <NavigationItem link ="/" active>
+        <NavigationItem link ="/" exact>
         Burger Builder
         </NavigationItem>
         <NavigationItem link ="/orders">
