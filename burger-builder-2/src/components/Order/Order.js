5befc40d8c715cddc81ff4873e9edf31346ab0ed
anukulSaini <anukulsaini2295@gmail.com>
2021-01-29 12:30:15 +0530

    added styling to Order.js

---
+++ burger-builder-2/src/components/Order/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Classes from './Order.js'
+
+const  order =(props) => {
+    return(
+          <div className={Classes.Order}>
+              <p>ingredients: salad(1)</p>
+          </div>
+    );
+}
+   
+ 
+export default order;
