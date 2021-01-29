4accf717d81779a10073c68e8c4c366ffbaa52bb
anukulSaini <anukulsaini2295@gmail.com>
2021-01-30 01:21:33 +0530

    imported axios in Order.js

--- burger-builder-2/src/containers/Orders/Orders.js
+++ burger-builder-2/src/containers/Orders/Orders.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-order';
 
 class Orders extends Component {
     state = {  }
+
+    componentDidMount () {
+        axios.get('https://react-my-burger-335c8-default-rtdb.firebaseio.com/ingredients2.json')
+             .then(response=>{
+            this.setState({ingredients:response.data});
+        })
+        .catch(error =>{
+            this.setState({error:true})
+        });
+     }
     render() { 
         return ( 
             <div>
