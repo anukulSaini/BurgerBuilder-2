0ccca2f115481393b8d8452104afa8ab4648873c
anukulSaini <anukulsaini2295@gmail.com>
2021-01-31 09:53:36 +0530

    switch case input for form

--- burger-builder-2/src/components/UI/Input/Input.js
+++ burger-builder-2/src/components/UI/Input/Input.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 const input = (props) => {
      
+      inputElement = null;
+
+      switch(props.inputType){
+          case('input'):
+          inputElement= <input {...props}></input>
+          break;
+          case('textarea'):
+          inputElement=<textarea{...props}></textarea>
+          break;
+          default:
+              inputElement=<input{...props}></input>
+      }
+
+
         return ( 
             <div></div>
          );
