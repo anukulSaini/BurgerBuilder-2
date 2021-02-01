5cbbb11e77fe19c6d4cd1f31a4da917cf49474fb
anukulSaini <anukulsaini2295@gmail.com>
2021-02-01 21:55:36 +0530

    fixed chekout componr=ent

--- burger-builder-2/src/containers/Checkout/Checkout.js
+++ burger-builder-2/src/containers/Checkout/Checkout.js
@@ -5,29 +5,25 @@ import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
-    state ={
-        ingredients :{salad: 1,
-            cheese: 1,
-            meat: 1,
-            bacon: 1},
-        price :11
+    state = {
+        ingredients: null,
+        price: 0
     }
 
-
-    // componentWillMount () {
-    //     const query = new URLSearchParams( this.props.location.search );
-    //     const ingredients = {};
-    //     let price = 0;
-    //     for ( let param of query.entries() ) {
-    //         // ['salad', '1']
-    //         if (param[0] === 'price') {
-    //             price = param[1];
-    //         } else {
-    //             ingredients[param[0]] = +param[1];
-    //         }
-    //     }
-    //     this.setState( { ingredients: ingredients, totalPrice: price } );
-    // }
+    componentWillMount () {
+        const query = new URLSearchParams( this.props.location.search );
+        const ingredients = {};
+        let price = 0;
+        for ( let param of query.entries() ) {
+            // ['salad', '1']
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
+        }
+        this.setState( { ingredients: ingredients, totalPrice: price } );
+    }
 
 
     chekouContinuedHandler= () =>{
