2d00b2482cedebf51e9e2c709e60a26a0ab09c9b
anukulSaini <anukulsaini2295@gmail.com>
2021-02-04 17:49:32 +0530

    updated code

--- burger-builder-2/src/components/Burger/Buildcontrols/Buildcontrols.js
+++ burger-builder-2/src/components/Burger/Buildcontrols/Buildcontrols.js
@@ -1,33 +1,31 @@
 import React from 'react';
 
-import classes from './Buildcontrols.css';
+import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
-
-const Controls =[
-   {label:'Salad',type:'salad'},
-   {label:'Meat',type:'meat'},
-   {label:'Cheese',type:'cheese'},
-   {label:'Bacon',type:'bacon'}
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
-const buildcontrols = (props) => (
-   <div className={classes.BuildControls}>
-      <p>Current Price : <strong>{props.price.toFixed(2)}</strong></p>
-       {Controls.map(ctrl =>(
-          <BuildControl 
-          key={ctrl.label} 
-          label ={ctrl.label}
-          added ={() => props.ingredientAdded(ctrl.type)}
-          remove ={() => props.ingredientremoved(ctrl.type)}
-          disabled={props.disabled[ctrl.type]}
-          ></BuildControl>
-       ))}
-       <button className={classes.OrderButton}
-        disabled={!props.purchasable}
-        onClick={props.ordered}
-        >ORDER NOW</button>
-   </div>
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
+        ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW</button>
+    </div>
 );
 
-export default buildcontrols;
+export default buildControls;
