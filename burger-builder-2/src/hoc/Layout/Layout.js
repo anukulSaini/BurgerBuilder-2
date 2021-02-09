6e134181da04ce0e32ba12a6e26b9a79c4dbf1b1
anukulSaini <anukulsaini2295@gmail.com>
2021-02-09 22:49:24 +0530

    changed Layout

--- burger-builder-2/src/hoc/Layout/Layout.js
+++ burger-builder-2/src/hoc/Layout/Layout.js
@@ -1,39 +1,38 @@
+import React, { Component } from 'react';
 
-import React, {Component} from 'react';
-import Aux from '../Aux';
+import Aux from '../Aux/Aux';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-class Layout extends Component{
-     state ={
-         showSideDrawer:true
-     }
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
 
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
 
-     sideDrawerClosedHandler = () =>{
-        this.setState({showSideDrawer:false});
-     }
-
-     drawerToggleClicked = () =>{
-         this.setState((prevState) => {
-         return {showSideDrawer: !prevState.showSideDrawer}
-        })
-     }
-
-    render (){
+    render () {
         return (
             <Aux>
-            <Toolbar drawerToggleClicked={this.drawerToggleClicked} />
-            <SideDrawer open ={this.state.showSideDrawer} 
-            closed={this.sideDrawerClosedHandler}/>
-           <main className={classes.Content}>
-               {this.props.children}
-           </main>
-           </Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
         )
     }
-} 
+}
 
 export default Layout;
