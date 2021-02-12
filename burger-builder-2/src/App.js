6a5c0bf448d495ea011cbdd95551c003f1124cc1
Anukul saini <anukulsaini2293@gmail.com>
2021-02-12 09:06:52 +0530

    Update App.js

--- burger-builder-2/src/App.js
+++ burger-builder-2/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+//By Anukul
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
